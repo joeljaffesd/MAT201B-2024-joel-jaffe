026afb46ba1799aa87b46d763dd79968232e3bb8
Joel Jaffe <joel@jaffesd.com>
2024-02-13 15:09:42 -0800

    Added 3D Particle Life

--- final-project-joel-jaffe/3dParticleLife.cpp
+++ final-project-joel-jaffe/3dParticleLife.cpp
@@ -13,8 +13,8 @@ using namespace std;
 
 struct Particle { // Particle struct
   int type;
-  Vec2f position; 
-  Vec2f velocity;
+  Vec3f position; 
+  Vec3f velocity;
 };
 
 float fMap(float value, float in_min, float in_max, float out_min, float out_max) { // custom mapping function
@@ -27,12 +27,14 @@ float fWrap(float input, float scope) { // custom wrapping function
     return lowerBound + fmodf((input - lowerBound + range), range);
 }
 
-Vec2f randomVec2f(float scale) { // <- Function that returns a Vec2f containing random coords
-  return Vec2f(rnd::uniformS(), rnd::uniformS()) * scale;
+Vec3f randomVec3f(float scale) { // <- Function that returns a Vec2f containing random coords
+  return Vec3f(rnd::uniformS(), rnd::uniformS(), rnd::uniformS()) * scale;
 } 
 
 struct MyApp : public App {
   Parameter simScale{"/simScale", "", 1.f, 0.00001f, 1.f}; // <- creates GUI parameter
+  Parameter springConstant{"/springConstant", "", 0.4, 0.0, 1.0}; // <- creates GUI parameter
+  Parameter radius{"/radius", "", 2.5, 0.0, 5.0}; // <- creates GUI parameter
   Mesh verts; // create mesh for visualzing particles
 
   static const int numTypes = 6; // numTypes
@@ -67,6 +69,8 @@ struct MyApp : public App {
     auto GUIdomain = GUIDomain::enableGUI(defaultWindowDomain());
     auto &gui = GUIdomain->newGUI();
     gui.add(simScale); // add parameter to GUI
+    gui.add(springConstant); // add parameter to GUI
+    //gui.add(radius); // add parameter to GUI
   }
 
   void onCreate() {
@@ -75,7 +79,7 @@ struct MyApp : public App {
     for (int i = 0; i < numParticles; i++) {  // for each iter...
       Particle particle; // initialzie a particle
       particle.type = rnd::uniformi(0, numTypes - 1); // give random type
-      particle.position = randomVec2f(simScale); // give random pos within simScale 
+      particle.position = randomVec3f(simScale); // give random pos within simScale 
       particle.velocity = 0; // give initial velocity of 0
       swarm.push_back(particle); // append to swarm vector
 
@@ -99,35 +103,24 @@ struct MyApp : public App {
     for (int i = 0; i < numParticles; i++) { // for each particle, ~60fps...
 
       float dis = 0; // initialize dis
-      Vec2f direction = 0; // initialize direction
-      Vec2f totalForce = 0; // initialize totalForce
-      Vec2f acceleration = 0; // initialize acceleration
+      Vec3f direction = 0; // initialize direction
+      Vec3f totalForce = 0; // initialize totalForce
+      Vec3f acceleration = 0; // initialize acceleration
+
+      float springForceMag = springConstant * (swarm[i].position.mag() - simScale);
+      Vec3f wtfDir = -swarm[i].position / swarm[i].position.mag();
+      acceleration += springForceMag * wtfDir;
       
       for (int j = 0; j < numParticles; j++) {
         if (i == j) {continue;} // don't have particles calculate forces on themselves
         direction *= 0; // set direction to 0
         direction += swarm[j].position; // direction == j particle 
         direction -= swarm[i].position; // direction -= current particle
-        
-        // make sure direction is measured with toroidal wrapping
-        if (direction[0] > simScale) { 
-          direction[0] -= 2 * simScale;
-        }
-        if (direction[0] < -1 * simScale) {
-          direction[0] += 2 * simScale;
-        }
-        if (direction[1] > simScale) {
-          direction[1] -= 2 * simScale;
-        }
-        if (direction[1] < -1 * simScale) {
-          direction[1] += 2 * simScale;
-        }
-        
         dis = direction.mag(); // Euclidian distance between particles
         direction.normalize(); // normalize to unit vector
 
         if (dis < minDistances[swarm[i].type][swarm[j].type]) { // separation forces (personal space)
-          Vec2f force = direction; 
+          Vec3f force = direction; 
           force *= abs(forces[swarm[i].type][swarm[j].type]) * -3; // calculate repulsion force based on type
           force *= fMap(dis, 0, minDistances[swarm[i].type][swarm[j].type], 1, 0); // map based on distance
           force *= K; // scale by K
@@ -135,7 +128,7 @@ struct MyApp : public App {
         } 
         
         if (dis < radii[swarm[i].type][swarm[j].type]) { // love/hate forces
-          Vec2f force = direction; 
+          Vec3f force = direction; 
           force *= forces[swarm[i].type][swarm[j].type]; // calculate force based on type
           force *= fMap(dis, 0, radii[swarm[i].type][swarm[j].type], 1, 0); // map based on distance (flip last two arguments?)
           force *= K; // scale by K
@@ -148,9 +141,6 @@ struct MyApp : public App {
       swarm[i].velocity += acceleration; // integrate acceleration
       swarm[i].velocity *= friction; // apply friction here?
       swarm[i].position += swarm[i].velocity; // integrate velocity
-      
-      swarm[i].position[0] = fWrap(swarm[i].position[0], simScale); // wrapping x-dim
-      swarm[i].position[1] = fWrap(swarm[i].position[1], simScale); // wrapping y-dim
       
       //swarm[i].velocity *= friction; // or apply friction here?
       verts.vertices()[i] = swarm[i].position; // skin mesh
@@ -171,11 +161,11 @@ struct MyApp : public App {
 
   void onDraw(Graphics &g) { 
     g.clear(0); // black background
-    g.camera(Viewpoint::IDENTITY); // Ortho [-1:1] x [-1:1]
+    //g.camera(Viewpoint::IDENTITY); // Ortho [-1:1] x [-1:1]
     g.pointSize(10); // set pointSize
     g.meshColor(); // color vertices based on type
     g.draw(verts); // draw verts
-    g.camera(Viewpoint::ORTHO_FOR_2D); // Ortho [0:width] x [0:height]
+    //g.camera(Viewpoint::ORTHO_FOR_2D); // Ortho [0:width] x [0:height]
   }
 };
 
